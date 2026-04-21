d16d1e61e60a6cc33703192e2656679e66166336
ejcondorf88 <eliancondor2523658@gmail.com>
2026-04-21 14:34:52 -0500

    fix : improvints whit deply

--- ms-core/src/presentation/quote/quote.module.ts
+++ ms-core/src/presentation/quote/quote.module.ts
@@ -5,19 +5,27 @@ import { CreateQuoteUseCase } from '../../application/quote/use-cases/create-quo
 import { GetQuotesUseCase } from '../../application/quote/use-cases/get-quotes.use-case';
 import { GetQuoteByIdUseCase } from '../../application/quote/use-cases/get-quote-by-id.use-case';
 import { UpdateQuoteUseCase } from '../../application/quote/use-cases/update-quote.use-case';
+import { CalculatePremiumUseCase } from '../../application/quote/use-cases/calculate-premium.use-case';
 import { QuoteRepositoryAdapter } from '../../infrastructure/quote/adapters/quote-repository.adapter';
 import { QuoteTypeOrmEntity } from '../../infrastructure/quote/entities/quote.typeorm.entity';
+import { PropertyTypeOrmEntity } from '../../infrastructure/property/entities/property.typeorm.entity';
+import { PremiumBreakdownTypeOrmEntity } from '../../infrastructure/premium/entities/premium-breakdown.typeorm.entity';
 import { StructuredLogger } from '../../common/logger/logger.service';
 import { QUOTE_REPOSITORY_PORT } from '../../domain/quote/ports/quote.repository.port';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([QuoteTypeOrmEntity])],
+  imports: [TypeOrmModule.forFeature([
+    QuoteTypeOrmEntity,
+    PropertyTypeOrmEntity,
+    PremiumBreakdownTypeOrmEntity,
+  ])],
   controllers: [QuoteController],
   providers: [
     CreateQuoteUseCase,
     GetQuotesUseCase,
     GetQuoteByIdUseCase,
     UpdateQuoteUseCase,
+    CalculatePremiumUseCase,
     StructuredLogger,
     {
       provide: QUOTE_REPOSITORY_PORT,
